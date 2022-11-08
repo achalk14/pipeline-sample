2c568cce0f41a3977489e52f683016d5cb3513b3
Achal Kumar <achalk14@users.noreply.github.com>
2022-11-08 15:50:12 -0500

    Added a Java Spring application

---
+++ demo-spring/src/main/java/com/example/demospring/model/Person.java
@@ -0,0 +1,28 @@
+package com.example.demospring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+public class Person {
+    private final UUID id;
+    // @NotBlank
+    @NotBlank
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ demo-spring/src/main/java/com/example/demospring/service/PersonService.java
@@ -0,0 +1,41 @@
+package com.example.demospring.service;
+
+import com.example.demospring.dao.PersonDao;
+import com.example.demospring.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> selectEveryone() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id){
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person updatedPerson) {
+        return personDao.modifyPersonById(id, updatedPerson);
+    }
+}
